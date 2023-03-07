c335a428a84d2765d2e8ba2e43204c20d0353bd9
Alihaitham26 <alihaithamahmed26@gmail.com>
2023-03-07 10:02:45 +0200

    start reading msgs from db

--- src/component/MsgViewer.jsx
+++ src/component/MsgViewer.jsx
@@ -1,18 +1,13 @@
+import { useContext } from "react";
+import { useAppContext } from "../context/appContext.jsx";
 import Msg from "./Msg.jsx";
 export default function MsgViewer(){
+  const AppContext = useAppContext()
+  console.log(AppContext.msgs)
+
   return(
     <div className="msg-viewer">
-      <Msg text="hello i am in the aaa lol you are not my phone bill you must do anything please" user="ali" time="2 minutes ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
-      <Msg text="hello" user="ahmed" time="minute ago"/>
+      {AppContext.msgs.map(({text,username:user,timestamp:time})=><Msg {...{text,user,time}} key={time+user}/>)}
     </div>
   )
 }
--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = createContext({})
 
@@ -7,10 +7,30 @@ const AppContextProvider = ({children}) =>{
   const supabaseUrl = import.meta.env["VITE_API_URL"]
   const supabaseKey = import.meta.env["VITE_API_KEY"]
   const supabase = createClient(supabaseUrl,supabaseKey)
+  const getMsgs = async () => await supabase.from("chat").select().range(0,49).order("id",{ascending:true})
+  const [msgs,setMsgs] = useState([])
+  useEffect(()=>{
+    getMsgs().then((({data})=>{setMsgs(data)}))
+    const chat = supabase.channel('custom-all-channel')
+    .on(
+      'postgres_changes',
+      { event: 'insert', schema: 'public', table: 'chat' },
+      (payload) => {
+        setMsgs((msgs)=>{
+          const copyMsg = Object.assign([],msgs)
+          copyMsg.push(payload.new)
+          return copyMsg
+        })
+      }
+    )
+    .subscribe()
+  },[])
+
   return (
     <AppContext.Provider
       value={{
-        supabase
+        supabase,
+        msgs
       }}
     >
       {children}
