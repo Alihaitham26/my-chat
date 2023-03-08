8a9a401c9050b4320b544b2c9dac1e49aa89b05a
Alihaitham26 <alihaithamahmed26@gmail.com>
2023-03-08 20:50:11 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import MsgSender from './component/MsgSender'
 import MsgViewer from './component/MsgViewer'
+import Navbar from './component/Navbar'
 import { AppContext, AppContextProvider } from './context/appContext'
 
 export default function App() {
   return (
     <AppContextProvider>
       <div className="App">
+        <Navbar/>
         <MsgViewer/>
         <MsgSender/>
       </div>
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+  return (
+    <nav>
+      <h1>my chat</h1>
+      <button className="source"><img src="./github.svg"></img> source</button>
+    </nav>
+  )
+}
+
