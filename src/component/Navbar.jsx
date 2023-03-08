21122596b145cb5074b0ae8dfca5ebede20dc97b
Alihaitham26 <alihaithamahmed26@gmail.com>
2023-03-08 20:53:54 +0200

    add githup link

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar(){
   return (
     <nav>
       <h1>my chat</h1>
-      <button className="source"><img src="./github.svg"></img> source</button>
+      <button className="source" onClick={()=>window.open("https://github.com/Alihaitham26/my-chat")}><img src="./github.svg"></img> source</button>
     </nav>
   )
 }
